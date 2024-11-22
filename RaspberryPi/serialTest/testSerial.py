eaaaa24af4cc680659a56c0b90d35c91b4860867
ndrs <andres_bava@hotmail.com>
2024-11-21 17:13:31 -0700

    testSerial.py ya reconoce el arduino sin tener que introducir el COM directo al codigo

--- RaspberryPi/serialTest/testSerial.py
+++ RaspberryPi/serialTest/testSerial.py
@@ -1,7 +1,19 @@
 
 import serial 
+import serial.tools.list_ports #https://pyserial.readthedocs.io/en/latest/tools.html#module-serial.tools.list_ports
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+puertos = list(serial.tools.list_ports.comports())
+
+available_ports = list(serial.tools.list_ports.comports())
+for port in available_ports:
+    print(port.description)
+    if port.description.find("R4") > 0: 
+        arduinoPort = port.device
+    else: print("N/A")
+print(arduinoPort)
+
+
+ser = serial.Serial(arduinoPort, 9600, timeout = 1)
 
 def read_arduino_data():
 	data = ser.readline().decode('ascii').strip()
