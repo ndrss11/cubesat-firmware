b7d2f272102d24062faf3b541454b3bbdad4d305
ndrs <andres_bava@hotmail.com>
2024-11-21 17:01:36 -0700

    Se agrega pequeño script para identificar el puerto a donde se conecta el UNO R4

---
+++ RaspberryPi/serialTest/usbSearch.py
@@ -0,0 +1,12 @@
+#Este codigo identifica el puerto en el que esta conectado el arduino UNO
+
+
+import serial.tools.list_ports
+
+available_ports = list(serial.tools.list_ports.comports())
+for port in available_ports:
+    print(port.description)
+    if port.description.find("R4") > 0: 
+        arduinoPort = port.device
+    else: print("N/A")
+print(arduinoPort)
