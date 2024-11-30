d742e849447d6fd55c31cec1ed73596e7de6c974
ndrs <andres_bava@hotmail.com>
2024-11-30 14:40:29 -0700

    juego con python

--- RaspberryPi/serialTest/testSerial.py
+++ RaspberryPi/serialTest/testSerial.py
@@ -2,6 +2,7 @@
 import serial 
 import serial.tools.list_ports #https://pyserial.readthedocs.io/en/latest/tools.html#module-serial.tools.list_ports
 import time
+import csv
 
 puertos = list(serial.tools.list_ports.comports())
 
@@ -14,7 +15,7 @@ for port in available_ports:
 print(arduinoPort)
 
 
-ser = serial.Serial(arduinoPort, 9600, timeout = 1)
+ser = serial.Serial(arduinoPort, 115200 , timeout = 1)
 
 def read_arduino_data():
 	data = ser.readline().decode('ascii').strip()
@@ -22,9 +23,26 @@ def read_arduino_data():
 		datoSensor = data.split(',')
 		print(datoSensor)
 
+def insertar_linea(nombre_archivo, texto):
+    try:
+        # Abre el archivo en modo de escritura al final (append)
+        with open(nombre_archivo, 'a') as archivo:
+            archivo.write(texto + '\n')  # Escribe la línea con salto de línea
+        print(f"Línea añadida: {texto}")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+
+# Uso del código
+nombre_del_archivo = 'mi_archivo.txt'
+linea_de_texto = 'Esta es una línea nueva.'
+
+# Llamada para insertar
+
+
 while True:
 	ser.write(bytes(b"cubesat"))
-	read_arduino_data()
-	line = ser.readline().decode('utf-8').rstrip()
+	line = ser.readline().decode('ascii').rstrip()
 	print(line)
-	time.sleep(5)	
+	insertar_linea(nombre_del_archivo, line)
+
+	time.sleep(2)	
